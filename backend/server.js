494db4cfcfdd35115fb9ed89dd42cb669692a860
Soondus <soondus1@hotmail.com>
2022-07-02 13:42:30 +0100

    set up server

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,6 @@ const http = require('http');
 
 const server = http.createServer((req,res) => {
     res.end('Welcome to Carbon green!');
-    console.log("Successfully connected to the server");
 
 });
 
