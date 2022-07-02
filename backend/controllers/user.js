f86d48505745379c0ec29234f58a3efb7abf71fe
Soondus <soondus1@hotmail.com>
2022-07-03 00:14:51 +0100

    Update user.js

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -26,8 +26,48 @@ exports.signup = (req, res, next) => {
     );
 };
 
-
+//can be accessed by both sides of the app
 exports.login = (req, res, next) => {
+    //make sure user exists
+    User.findOne({ email: req.body.email }).then(
+        (user) => {
+            if (!user) {
+                return res.status(401).json({
+                    error: new Error('User not found!')
+                });
+            }
+            //user exists, check password
+            bcrypt.compare(req.body.password, user.password).then(
+                (valid) => {
+                    if (!valid) {
+                        return res.status(401).json({
+                            error: new Error('Invalid password!')
+                        });
+                    }
+                    //password is correct, send token
+                    res.status(200).json({
+                        userId: user._id,
+                        token:'token' //generic token for now
+                    });
+                }
+                //if bcrypt goes wrong send back error
+            ).catch(
+                (error) => {
+                    res.status(500).json({
+                        error: error
+                    });
+                }
+            );
+        }
+        //if user does not exist send back error
+    ).catch(
+        (error) => {
+            res.status(500).json({
+                error: error
+            });
+        }
+    );
+
 };
 
 //no log out as using token based authentication (more secure than sessions and tokens)
