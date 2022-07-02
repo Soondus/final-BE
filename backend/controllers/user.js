ad50ef042aef4b7b28682da288d143dea60107c9
Soondus <soondus1@hotmail.com>
2022-07-03 00:04:36 +0100

    added user authentication

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,8 +1,29 @@
+const bcrypt = require('bcrypt');
 //import user model
 const User = require('../models/user');
 
-exports.signup = (req, res, next) => {
 
+exports.signup = (req, res, next) => {
+    bcrypt.hash(req.body.password, 10).then(
+        (hash) => {
+            const user = new User({
+                email: req.body.email,
+                password: hash
+            });
+            user.save().then(() => {
+                res.status(201).json({
+                    message: 'User added successfully!'
+                });
+            }
+            ).catch(
+                (error) => {
+                    res.status(400).json({
+                        error: error
+                    });
+                }
+            );
+        }
+    );
 };
 
 
